b94aa1d626c031ecb886ba299d38c6d825e54340
daniels8945 <obaj.daniels@gmail.com>
2025-05-19 13:13:14 +0100

    Changes made

--- src/Dashboard/Allproducts.jsx
+++ src/Dashboard/Allproducts.jsx
@@ -18,7 +18,7 @@ function Products(){
         <div className="flex flex-col gap-8">
            <div  className="gap-2 z-10 sticky top-0 px-4 xl:px-12 shadow-md flex flex-col xl:flex-row xl:items-center-safe justify-center xl:justify-between bg-white w-full h-[100px]">
                 <h1 className="text-4xl font-semibold font-raleway text-green-500">All available products</h1>
-                <Link to="/addproducts" className="flex items-center text-2xl font-bold font-raleway gap-4 pt-4 cursor-pointer hover:underline"> Add Products <FaExternalLinkAlt className="text-green-500" /> </Link>
+                <Link to="/addproducts" className="flex items-center text-[20px] font-semibold font-raleway gap-4 pt-4 cursor-pointer hover:underline"> Add Products <FaExternalLinkAlt className="text-green-500" /> </Link>
            </div>
 
             { products && products.map((product, id) => (
--- src/components/ui/ProductCard.jsx
+++ src/components/ui/ProductCard.jsx
@@ -6,9 +6,9 @@ return (
         <div className="relative h-[400px] bg-white border-1 border-neutral-200 shadow-md rounded-lg flex flex-col items-center">
            <img src={image} alt="product image" className="rounded-t-md h-[220px] w-full object-cover hover:scale-[1.015]"/>
           <div className=" flex flex-col w-full p-4 gap-2">
-            <h3 className="break-all text-2xl font-semibold font-raleway">{name}</h3>
+            <h3 className="break-all lg:text-lg font-semibold font-raleway">{name}</h3>
             <p className="break-all text-gray-500 font-raleway truncate">{description}</p>
-            <Link to={"/exploreproducts"} className="break-all cursor-pointer text-black mt-4 font-bold font-raleway flex items-center gap-4">View {name} details<IoArrowForward className="text-green-500 text-lg" /></Link>
+            <Link to={"/exploreproducts"} className="break-all cursor-pointer text-black mt-4 text-md truncate font-bold font-raleway flex items-center gap-4">View {name} details<IoArrowForward className="text-green-500 text-lg" /></Link>
           </div>
         </div>
   );
